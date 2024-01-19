506c1ee638eb227ba9108bd36b50b0cddddd5d49
RenyBlanco <rey.blanco@yahoo.com>
2024-01-19 11:32:59 -0300

    Incluye GA4

--- App/Views/Templates/Header.php
+++ App/Views/Templates/Header.php
@@ -20,8 +20,8 @@
 
     <!-- Theme style -->
     <link rel="stylesheet" href="<?= ASSETS . '/dist/css/adminlte.min.css' ?>">
-    <!-- Etiqueta global del sitio (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"></script>
+    <!-- Etiqueta global del sitio (gtag.js) - Google Analytics 
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-WGY7SN00LJ"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
 
@@ -29,13 +29,38 @@
             dataLayer.push(arguments);
         }
         gtag('js', new Date());
-        gtag('config', 'G-XXXXXXXXXX', {
+        gtag('config', 'G-WGY7SN00LJ', {
             'send_page_view': true
         });
     </script>
+    -->
+
+    <!-- Google Tag Manager 
+        -->
+    <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-KWL59JNL');
+    </script>
+    <!-- End Google Tag Manager -->
+
 </head>
 
 <body class="hold-transition sidebar-mini layout-fixed">
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KWL59JNL" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <!-- Site wrapper -->
     <div class="wrapper">
         <!-- Navbar -->
